e8c5351832f910c4805f878ed0ed9f01aeaab59c
Medha Adepu <medha.adepucse@gmail.com>
2026-03-30 00:30:01 +0530

    push 2 frontend

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://erp-po-system-ur32.onrender.com";
 
 async function loadPurchaseOrders() {
     const tableBody = document.getElementById("poTableBody");
--- frontend/create-po.js
+++ frontend/create-po.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://erp-po-system-ur32.onrender.com";
 
 const vendorSelect = document.getElementById("vendorSelect");
 const itemsContainer = document.getElementById("itemsContainer");
